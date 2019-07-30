5b2ec3d1e4151efc3bb49df0c7a56cb461b7fbe2
Administrator <Administrator@windows10.microdone.cn>
2019-07-30 14:22:16 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,6 +3,6 @@ package helloworld;
 public class HelloWorld {
  
     public static void main(String[] args) {
-        System.out.println("Hello World  hlzs");
+        System.out.println("Hello World 你妈死了说");
     }
 }
